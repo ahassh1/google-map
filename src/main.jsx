492236923f2603271ec7310fd90ca34257fcd661
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-21 11:07:02 +0600

    google map api integration but dont use any key

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { APIProvider } from '@vis.gl/react-google-maps'
+import { googleMapAPIKey } from './map-api-key.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <APIProvider apiKey={googleMapAPIKey}>
+      <App />
+    </APIProvider>
   </StrictMode>,
 )
